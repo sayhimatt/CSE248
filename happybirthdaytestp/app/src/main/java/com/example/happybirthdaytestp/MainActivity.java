95aab402a0a6359940c11c6e54bed2ca42544690
sayhimatt <mattsayshi@hotmail.com>
2019-10-20 23:26:02 -0400

    failing miserably
    at doing the XML code lol

--- happybirthdaytestp/app/src/main/java/com/example/happybirthdaytestp/MainActivity.java
+++ happybirthdaytestp/app/src/main/java/com/example/happybirthdaytestp/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.happybirthdaytestp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView test;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
