9a6ba2e97f391ad2c4cee2c7b29acfacd69f4ee0
sayhimatt <mattsayshi@hotmail.com>
2019-11-24 15:54:54 -0500

    Begun Firebase integration
    More edits to login and registry so everything looks nice. My way.

--- CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
+++ CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
@@ -5,20 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import util.OnClickAssigner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        OnClickAssigner.loginHandler(findViewById(R.id.login_image_button));
+        OnClickAssigner.loginHandler(findViewById(R.id.main_login_linear_layout), mAuth);
+
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser cUser = mAuth.getCurrentUser();
+        //updateUI(cUser);
 
     }
 
 
-
+    public boolean backToLogin(View v){
+        setContentView(R.layout.activity_login);
+        return true;
+    }
     public boolean registerAccount(View v){
         setContentView(R.layout.activity_register);
         return true;
--- CollegeSearchAppProject/app/src/main/java/util/OnClickAssigner.java
+++ CollegeSearchAppProject/app/src/main/java/util/OnClickAssigner.java
@@ -1,28 +1,54 @@
 package util;
 
 import android.content.DialogInterface;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.guidi.collegesearch.main.R;
 
 import static android.widget.Toast.makeText;
 
 public final class OnClickAssigner {
-    public static void loginHandler(View v){
+    public static void loginHandler(View v, FirebaseAuth mAuth){
         ImageButton loginB = v.findViewById(R.id.login_image_button);
+        final View v1 = v;
+        final FirebaseAuth userAuthen = mAuth;
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "This", Toast.LENGTH_LONG).show();
+                Log.d("HEY MATT WHAT UP", "you got mail");
+                String emailText = ((EditText)v1.findViewById(R.id.email_edit_text)).getText().toString();
+                String pText = ((EditText)v1.findViewById(R.id.password_edit_text)).getText().toString();
+                Log.d("HEY MATT WHAT UP", emailText);
+                Log.d("HEY MATT WHAT UP", pText);
+                userAuthen.signInWithEmailAndPassword(emailText, pText)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Log.d("Login Test" ,"signInWithEmail:success");
+
+                                }else {
+                                    Log.w("Login Test", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(v1.getContext(), "Authentication failed sucka", Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
 
             }
         });
-
-        v.findViewById(R.id.email_edit_text);
-        v.findViewById(R.id.password_edit_text);
     }
 }
 
