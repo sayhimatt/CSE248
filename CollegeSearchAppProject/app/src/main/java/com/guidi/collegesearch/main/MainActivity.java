23cb224a51b3cfd48e5a9bd6ed21c0f75df8ed95
sayhimatt <mattsayshi@hotmail.com>
2019-11-24 21:03:20 -0500

    User integration is cloud-based!
    Holy poop it works!

--- CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
+++ CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
@@ -1,14 +1,25 @@
 package com.guidi.collegesearch.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import util.OnClickAssigner;
+import util.ToastyMatt;
+
+import static util.OnClickAssigner.loginHandler;
+import static util.OnClickAssigner.registrationHandler;
+import static util.OnClickAssigner.setOnClickAssigner;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -17,14 +28,13 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        OnClickAssigner.loginHandler(findViewById(R.id.main_login_linear_layout), mAuth);
-
-
+        OnClickAssigner.setOnClickAssigner(findViewById(R.id.main_login_linear_layout), mAuth);
+        OnClickAssigner.loginHandler();
     }
     @Override
     public void onStart(){
         super.onStart();
-        FirebaseUser cUser = mAuth.getCurrentUser();
+        //FirebaseUser cUser = mAuth.getCurrentUser();
         //updateUI(cUser);
 
     }
@@ -32,10 +42,16 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean backToLogin(View v){
         setContentView(R.layout.activity_login);
+        OnClickAssigner.setOnClickAssigner(findViewById(R.id.main_login_linear_layout), mAuth);
+        loginHandler();
         return true;
     }
     public boolean registerAccount(View v){
         setContentView(R.layout.activity_register);
+        Button rButton = (Button)findViewById(R.id.register_account_button);
+        OnClickAssigner.setOnClickAssigner(findViewById(R.id.root_registration_view), mAuth);
+        registrationHandler(findViewById(R.id.root_registration_view), rButton);
         return true;
     }
+
 }
--- CollegeSearchAppProject/app/src/main/java/util/OnClickAssigner.java
+++ CollegeSearchAppProject/app/src/main/java/util/OnClickAssigner.java
@@ -2,6 +2,7 @@ package util;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -11,42 +12,78 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.guidi.collegesearch.main.R;
 
 import static android.widget.Toast.makeText;
 
 public final class OnClickAssigner {
-    public static void loginHandler(View v, FirebaseAuth mAuth){
-        ImageButton loginB = v.findViewById(R.id.login_image_button);
-        final View v1 = v;
-        final FirebaseAuth userAuthen = mAuth;
+    private static View rootV;
+    private static FirebaseAuth mAuth;
+    public static void setOnClickAssigner(View overView, FirebaseAuth firebaseAuthentication){
+        rootV = overView;
+        mAuth = firebaseAuthentication;
+    }
+    public static void loginHandler(){
+        ImageButton loginB = rootV.findViewById(R.id.login_image_button);
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailText = ((EditText)v1.findViewById(R.id.email_edit_text)).getText().toString();
-                String pText = ((EditText)v1.findViewById(R.id.password_edit_text)).getText().toString();
+                String emailText = ((EditText)rootV.findViewById(R.id.email_edit_text)).getText().toString();
+                String pText = ((EditText)rootV.findViewById(R.id.password_edit_text)).getText().toString();
                 try{
-                    userAuthen.signInWithEmailAndPassword(emailText, pText)
+                    mAuth.signInWithEmailAndPassword(emailText, pText)
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Log.d("Login Test", "signInWithEmail:success");
-                                        ToastyMatt.makeMToast(v1, "Login successful", true).show();
+                                        ToastyMatt.makeMToast(rootV, "Login successful", true).show();
                                     } else {
                                         Log.w("Login Test", "signInWithEmail:failure", task.getException());
-                                        ToastyMatt.makeMToast(v1, "Sorry Mate wrong login info", true).show();
+                                        ToastyMatt.makeMToast(rootV, "Sorry mate wrong login info", true).show();
 
                                     }
                                 }
                             });
                 }catch(IllegalArgumentException e){
-                    ToastyMatt.makeMToast(v1,
-                            "Sorry Mate wrong login info\nAre you missing something?", true).show();
+                    ToastyMatt.makeMToast(rootV,
+                            "Sorry mate wrong login info\nAre you missing something?", true).show();
                 }
 
             }
         });
     }
+    public static void registrationHandler(View v, Button rButton) {
+        rButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText)(rootV.findViewById(R.id.email_address_editText))).getText().toString();
+                String password = ((EditText)(rootV.findViewById(R.id.password_one_editText))).getText().toString();
+                String password2 = ((EditText)(rootV.findViewById(R.id.password_two_editText))).getText().toString();
+                if(password.equals(password2)) {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Test", "hello");
+                            ToastyMatt.makeMToast(rootV, "It worked", true).show();
+                            //FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("Failure", "hello");
+                            ToastyMatt.makeMToast(rootV, "It failed", true).show();
+                        }
+
+                        // ...
+                        }
+                    });
+                }
+            }
+        });
+    }
+
 }
 
