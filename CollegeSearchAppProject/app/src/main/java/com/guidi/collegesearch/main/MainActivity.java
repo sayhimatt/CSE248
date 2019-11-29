d0e51564431f162d59b185acc28b08b93c3ba9d5
sayhimatt <mattsayshi@hotmail.com>
2019-11-29 02:21:47 -0500

    Trying to get the database running

--- CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
+++ CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import util.OnClickAssigner;
 import util.ToastyMatt;
@@ -23,14 +30,30 @@ import static util.OnClickAssigner.setOnClickAssigner;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private FirebaseDatabase userDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        OnClickAssigner.setOnClickAssigner(findViewById(R.id.main_login_linear_layout), mAuth);
-        OnClickAssigner.loginHandler();
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseFirestore.setLoggingEnabled(true);
+        Log.d("Test error", "what happened");
+        DatabaseReference myRef = database.getReference("message");
+        Log.d("Test error", "what happened");
+        myRef.push().setValue("Hello, World!", new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference dataReference) {
+                Log.d("Test error", "what happened" + databaseError.getMessage());
+            }
+
+        });
+        Log.d("Test error", "what happened2222");
+        //OnClickAssigner.setOnClickAssigner(findViewById(R.id.main_login_linear_layout), mAuth);
+        //OnClickAssigner.loginHandler();
+
     }
+
     @Override
     public void onStart(){
         super.onStart();
--- CollegeSearchAppProject/app/src/main/java/model/Account.java
+++ CollegeSearchAppProject/app/src/main/java/model/Account.java
@@ -3,6 +3,12 @@ package model;
 public class Account  {
     private Username uN;
     private Password pW;
+    private String firstName, lastName;
+    private static final int MIN_SAT_SCORE = 400, MAX_SAT_SCORE = 1600, MIN_ACT_SCORE = 1,MAX_ACT_SCORE = 36;
+    /*Minimum SAT score is 400 Max SAT score is 1600
+     Minimum ACT score is 1 Max ACT score is 36
+     Let's make 'em final
+     I'm lazy, and don't want to make a filter so if I get passed values outside these limits I set it to max/min*/
     public Account(Username username, Password password){
         this.uN = username;
         this.pW = password;
