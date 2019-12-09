372132b65b33d5ff37795c6551cd2d5c3a59131f
sayhimatt <mattsayshi@hotmail.com>
2019-12-08 23:15:21 -0500

    College Database
    COMPLETE woooooohooo. Time to work on searches in database and complete the search layouts.

--- CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
+++ CollegeSearchAppProject/app/src/main/java/com/guidi/collegesearch/main/MainActivity.java
@@ -3,17 +3,46 @@ package com.guidi.collegesearch.main;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 import custom_views.CheckboxSpinner;
+import custom_views.mToast;
+import model.Account;
+import model.School;
+import model.Username;
 import util.OnClickAssigner;
 import util.State;
 
@@ -21,8 +50,13 @@ import static util.OnClickAssigner.loginHandler;
 import static util.OnClickAssigner.registrationHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int maxPageN;
+    private static int pageN;
+    private static int numOfSchoolsFound;
     private FirebaseAuth mAuth;
     private FirebaseDatabase userDatabase;
+    private RequestQueue testQueue;
+    private RequestQueue betterQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         OnClickAssigner.setOnClickAssigner(findViewById(R.id.main_login_linear_layout), mAuth, this);
         OnClickAssigner.loginHandler();
-
+        numOfSchoolsFound = 0;
         mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -43,7 +77,33 @@ public class MainActivity extends AppCompatActivity {
             Log.d("whoAmISignedInAs?", mAuth.getCurrentUser().getEmail());
             goToMainActivity(this.findViewById(R.id.main_login_linear_layout));
 
+            testQueue = Volley.newRequestQueue(this);
+            getTotalPagesToParse();
+            testQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+                @Override
+
+                public void onRequestFinished(Request<Object> request) {
+                    pageN--;
+                    betterQueue = Volley.newRequestQueue(getBaseContext());
+                    betterQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+                        @Override
+                        public void onRequestFinished(Request<Object> request) {
+                            Log.d("Wow" , "I found this many schools " + numOfSchoolsFound);
+                        }
+                    });
+                    for(int i = 0; i <= maxPageN; i++) {
+                        Log.d("Page #", "" + i);
+                        jsonParse();
+                    }
+
+
+                }
+            });
+
         }
+
+
+
 
     }
     @Override
@@ -88,5 +148,117 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+    private void getTotalPagesToParse(){
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, getQueryForm(), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject metadata = response.getJSONObject("metadata");
+                            int i = metadata.getInt("total");
+                            double d = (i / 100);
+                            i = (int)(d + 1);
+                            maxPageN = i;
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+
+        });
+
+        testQueue.add(request);
+    }
+    private void jsonParse() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, getQueryForm(), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject metadata = response.getJSONObject("metadata");
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject school = jsonArray.getJSONObject(i);
+                                School newSchoolToEnter = new School();
+                                try {
+                                    newSchoolToEnter.setId(school.getString("id"));
+                                    newSchoolToEnter.setCollegeName(school.getString("school.name"));
+                                    newSchoolToEnter.setCity(school.getString("school.city"));
+                                    newSchoolToEnter.setZip(school.getString("school.zip"));
+                                    newSchoolToEnter.setStateAbr(school.getString("school.state"));
+                                    newSchoolToEnter.setSchoolURL(school.getString("school.school_url"));
+                                    newSchoolToEnter.setCostIn(school.getString("latest.cost.tuition.in_state"));
+                                    newSchoolToEnter.setCostOut(school.getString("latest.cost.tuition.out_of_state"));
+                                    newSchoolToEnter.setSatW25(school.getString("latest.admissions.sat_scores.25th_percentile.writing"));
+                                    newSchoolToEnter.setSatW75(school.getString("latest.admissions.sat_scores.75th_percentile.writing"));
+                                    newSchoolToEnter.setSatM25(school.getString("latest.admissions.sat_scores.25th_percentile.math"));
+                                    newSchoolToEnter.setSatM75(school.getString("latest.admissions.sat_scores.75th_percentile.math"));
+                                    newSchoolToEnter.setSatR25(school.getString("latest.admissions.sat_scores.25th_percentile.critical_reading"));
+                                    newSchoolToEnter.setSatR75(school.getString("latest.admissions.sat_scores.75th_percentile.critical_reading"));
+                                    newSchoolToEnter.setRegID(school.getString("school.region_id"));
+                                    newSchoolToEnter.setStudentSize(school.getString("latest.student.size"));
+                                    newSchoolToEnter.setMainDegree(school.getString("school.degrees_awarded.predominant"));
+                                    newSchoolToEnter.setAdmRate(school.getString("latest.admissions.admission_rate.overall"));
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference myRef = database.getReference("schools");
+                                    myRef.child(String.valueOf(newSchoolToEnter.getId())).setValue(newSchoolToEnter);
+                                    numOfSchoolsFound++;
+                                }catch(JSONException e){
+
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 200,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        betterQueue.add(request);
+
+    }
+
+    private String getQueryForm(){
+        final String API_KEY = "Afw22hMz3Vc46y5Qps2Zrhr9VcpyEec383DD1dBl";
+        int page = pageN;
+        String qForm = "https://api.data.gov/ed/collegescorecard/v1/schools.json?&fields=" +
+                "id," +
+                "school.region_id," +
+                "school.name," +
+                "school.city," +
+                "school.state," +
+                "school.zip," +
+                "school.school_url," +
+                "latest.cost.tuition.in_state," +
+                "latest.cost.tuition.out_of_state," +
+                "latest.student.size," +
+                "latest.admissions.admission_rate.overall," +
+                "school.degrees_awarded.predominant," +
+                "latest.admissions.sat_scores.25th_percentile.critical_reading," +
+                "latest.admissions.sat_scores.25th_percentile.math," +
+                "latest.admissions.sat_scores.25th_percentile.writing," +
+                "latest.admissions.sat_scores.75th_percentile.critical_reading," +
+                "latest.admissions.sat_scores.75th_percentile.math," +
+                "latest.admissions.sat_scores.75th_percentile.writing," +
+                "&api_key=" + API_KEY +
+                "&_per_page=100" + "&page=" + page
+                + "&sort=school.name";
+        pageN++;
+        return qForm;
+    }
+
+
 
 }
---
+++ CollegeSearchAppProject/app/src/main/java/model/School.java
@@ -0,0 +1,200 @@
+package model;
+
+public class School {
+    private int id, costIn, costOut, regID, studentSize, mainDegree;
+    private String collegeName, city, stateAbr, schoolURL, zip;
+    private double admRate, satW25, satW75, satM25,
+            satM75, satR25, satR75;
+    public School(){
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = Integer.parseInt(id);
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+       this.zip = zip;
+    }
+
+    public int getCostIn() {
+        return costIn;
+    }
+
+    public void setCostIn(String costIn) {
+        if(costIn == "null"){
+            this.costIn = 0;
+        }else {
+            this.costIn = Integer.parseInt(costIn);
+        }
+    }
+
+    public int getCostOut() {
+        return costOut;
+    }
+
+    public void setCostOut(String costOut) {
+        if(costOut == "null"){
+            this.costOut = 0;
+        }else {
+            this.costOut = Integer.parseInt(costOut);
+        }
+    }
+
+    public Double getSatW25() {
+        return satW25;
+    }
+
+    public void setSatW25(String satW25) {
+        if(satW25 == "null"){
+            this.satW25 = 0;
+        }else {
+            this.satW25 = Double.parseDouble(satW25);
+        }
+    }
+
+    public Double getSatW75() {
+        return satW75;
+    }
+
+    public void setSatW75(String satW75) {
+        if(satW75 == "null"){
+            this.satW75 = 0;
+        }else {
+            this.satW75 = Double.parseDouble(satW75);
+        }
+    }
+
+    public Double getSatM25() {
+        return satM25;
+    }
+
+    public void setSatM25(String satM25) {
+        if(satM25 == "null"){
+            this.satM25 = 0;
+        }else {
+            this.satM25 = Double.parseDouble(satM25);
+        }
+    }
+
+    public Double getSatM75() {
+        return satM75;
+    }
+
+    public void setSatM75(String satM75) {
+        if(satM75 == "null"){
+            this.satM75 = 0;
+        }else {
+            this.satM75 = Double.parseDouble(satM75);
+        }
+    }
+
+    public Double getSatR25() {
+        return satR25;
+    }
+
+    public void setSatR25(String satR25) {
+        if(satR25 == "null"){
+            this.satR25 = 0;
+        }else {
+            this.satR25 = Double.parseDouble(satR25);
+        }
+    }
+
+    public Double getSatR75() {
+        return satR75;
+    }
+
+    public void setSatR75(String satR75) {
+        if(satR75 == "null"){
+            this.satR75 = 0;
+        }else {
+            this.satR75 = Double.parseDouble(satR75);
+        }
+    }
+
+    public int getRegID() {
+        return regID;
+    }
+
+    public void setRegID(String regID) {
+        if(regID == "null"){
+            this.regID = 0;
+        }else {
+            this.regID = Integer.parseInt(regID);
+        }
+    }
+
+    public int getStudentSize() {
+        return studentSize;
+    }
+
+    public void setStudentSize(String studentSize) {
+        if(studentSize == "null"){
+            this.studentSize = 0;
+        }else {
+            this.studentSize = Integer.parseInt(studentSize);
+        }
+    }
+    public int getMainDegree() {
+        return mainDegree;
+    }
+
+    public void setMainDegree(String mainDegree) {
+        if(mainDegree == "null"){
+            this.mainDegree = 0;
+        }else {
+            this.mainDegree = Integer.parseInt(mainDegree);
+        }
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateAbr() {
+        return stateAbr;
+    }
+
+    public void setStateAbr(String stateAbr) {
+        this.stateAbr = stateAbr;
+    }
+
+    public String getSchoolURL() {
+        return schoolURL;
+    }
+
+    public void setSchoolURL(String schoolURL) {
+        this.schoolURL = schoolURL;
+    }
+
+    public double getAdmRate() {
+        return admRate;
+    }
+    public void setAdmRate(String admRate) {
+        if(admRate == "null"){
+            this.admRate = 0.0;
+        }else{
+            this.admRate = Double.parseDouble(admRate);
+        }
+    }
+}
